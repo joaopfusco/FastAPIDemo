248d02d0a8569f8d8af4a2dd99aab50ed695d685
JPFUSCO_LAPTOP\joaop <joaopedrofusco@gmail.com>
2025-03-06 09:31:43 -0300

    Implement user CRUD operations and login endpoint in user router

--- routers/user_router.py
+++ routers/user_router.py
@@ -1,22 +1,36 @@
+from fastapi import Depends
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
+from sqlalchemy.orm import Session
 from db.database import get_db
 from models.user import User
-from schemas.user_schema import UserSchema
+from schemas.user_schema import LoginSchema, UserSchema
 from routers.base_router import BaseRouter
 from services.user_service import UserService
 
-# router = SQLAlchemyCRUDRouter(
-#     schema=UserSchema,
-#     create_schema=UserSchema,
-#     update_schema=UserSchema, 
-#     db_model=User,
-#     db=get_db,
-#     prefix="/users",
-#     tags=["Users"]
-# )
+router = SQLAlchemyCRUDRouter(
+    schema=UserSchema,
+    create_schema=UserSchema,
+    update_schema=UserSchema, 
+    db_model=User,
+    db=get_db,
+    prefix="/users",
+    tags=["Users"]
+)
 
-class UserRouter(BaseRouter[User, UserService, UserSchema]):
-    def __init__(self):
-        super().__init__(model=User, service=UserService, prefix="/users", tag="Users")
+service = UserService()
 
-router = UserRouter().get_router()
+@router.get("", response_model=list[UserSchema])
+def get_users(db: Session = Depends(get_db)):
+    return service.get_all(db)
+
+@router.post("", response_model=UserSchema)
+def create_user(user: UserSchema, db: Session = Depends(get_db)):
+    return service.create(db, user)
+
+@router.put("/{item_id}", response_model=UserSchema)
+def update_user(item_id: int, user: UserSchema, db: Session = Depends(get_db)):
+    return service.update(db, item_id, user)
+
+@router.post("/login", response_model=LoginSchema)
+def login(user: UserSchema, db: Session = Depends(get_db)):
+    return
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -4,3 +4,7 @@ from schemas.entity_schema import EntitySchema
 class UserSchema(EntitySchema):
     username: str
     password: str
+
+class LoginSchema(BaseModel):
+    token: str
+    user: UserSchema
