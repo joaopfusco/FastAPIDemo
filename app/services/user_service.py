17261b28ee3aa3658996c342170ec8f00151c3ee
JPFUSCO_LAPTOP\joaop <joaopedrofusco@gmail.com>
2025-04-06 12:04:56 -0300

    Instantiate UserService in UserRouter and initialize UserService with User model in UserService class

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,11 @@
 from sqlalchemy.orm import Session
+from app.models.user import User
 from app.services.base_service import BaseService
 
 class UserService(BaseService):
+    def __init__(self):
+        super().__init__(User)
+
     def get(self, session: Session):
         print("Override no get")
         return super().get(session)
