ca6fd03abec4e27575510029202176b949cc1598
JPFUSCO_LAPTOP\joaop <joaopedrofusco@gmail.com>
2025-04-06 23:11:12 -0300

    Add SQLite foreign key support in database connection

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
@@ -7,6 +7,14 @@ DATABASE_URL = config("DATABASE_URL", default="sqlite:///./db.sqlite3")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+if DATABASE_URL.startswith("sqlite"):
+    @event.listens_for(engine, "connect")
+    def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
